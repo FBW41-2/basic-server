4eb618ce91c5908f9a842af1b8d11fbe142dc061
galymax <werkhowski@gmail.com>
2021-03-22 11:00:17 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// import predefined http module from node
+const http = require('http');
+
+// listener that waits for requests
+const server = http.createServer((request, response) => {
+    response.write("<h1>Hello Browser</h1>")
+    response.end()
+})
+
+// start the server / wait for requests on port 8080
+server.listen(8080)
